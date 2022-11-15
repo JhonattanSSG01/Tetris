23502c8b61b5236d8a57c4305e0bc70530fa58a8
Jhonattan Saavedra Gomez <jhonattanestivensaavedragomez@gmail.com>
2022-11-15 12:04:36 -0500

    Creando las filas y columnas de la cuadricula

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ PINCEL.scale(20,20);
 // 200 / 20 = 10
 // 400 / 20 = 20
 // 10 columnas y 20 filas
+
+// La funcion createMatriz creara la matriz con el ancho y alto que llegue como parametro
+function createMatriz(width, height) {
+  const MATRIZ = []; // Se declara e inicializa la constante con una lista vacia
+
+  // El ciclo while nos ayudara que cada vez que el heigth se le resta 1(sea true), se creara una nueva lista dentro del array vacio hasta llegar a 0 que devuelve false
+  // 0 = false
+  // Numeros mayores a 0 son true
+  while (height--) {
+    MATRIZ.push(new Array(width).fill(0)); // Agrega una nueva lista dentro de la lista vacia creada anteriormente
+  }
+
+  // console.table(MATRIZ); Se visualiza en la consola en formato tabla
+
+  return MATRIZ; // Devuelve la matriz ya construida 
+}
